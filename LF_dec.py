1ffb89542049cf697fb7c0a883f3f52daa9cb4ba
charlesll <charles.lelosq@gmail.com>
2014-07-30 13:58:14 -0500

    Update LF_dec.py
    deleting the peak_melt and peak_fluid functions because useless... calculation of individual peaks is done in the residual_melt and residual_fluid functioms

--- LF_dec.py
+++ LF_dec.py
@@ -17,7 +17,7 @@ Put it anywhere, you need however the lib-charles library as well as numpy, scip
 that usually come with any python distribution
 """
 import sys
-sys.path.append("/Users/charleslelosq/anaconda/lib/python2.7/lib-charles")
+sys.path.append("/Users/Celita/Desktop/RamPy-master/lib-charles/")
 
 import csv
 import numpy as np
@@ -28,7 +28,7 @@ from pylab import *
 from StringIO import StringIO
 from scipy import interpolate
 
-# to fit spectra we use the lmfit software of Max Newville, CARS, university of Chicago, available on the web
+# to fit spectra we use the lmfit software of Matt Newville, CARS, university of Chicago, available on the web
 from lmfit import minimize, Minimizer, Parameters, Parameter, report_fit, fit_report
 
 from spectratools import *  #Charles' libraries and functions
@@ -66,14 +66,17 @@ def residual_melt(pars, x, data=None, eps=None):
     l5 = pars['l5'].value
     
     # Gaussian model
-    model = gaussian(x,a1,f1,l1) + \
-            gaussian(x,a2,f2,l2) + \
-            gaussian(x,a3,f3,l3) + \
-            gaussian(x,a4,f4,l4) + \
-            gaussian(x,a5,f5,l5)
+    
+    peak1 = gaussian(x,a1,f1,l1)
+    peak2 = gaussian(x,a2,f2,l2)
+    peak3 = gaussian(x,a3,f3,l3)
+    peak4 = gaussian(x,a4,f4,l4)
+    peak5 = gaussian(x,a5,f5,l5)    
+    
+    model = peak1 + peak2 + peak3 + peak4 + peak5
     
     if data is None:
-        return model
+        return model, peak1, peak2, peak3, peak4, peak5
     if eps is None:
         return (model - data)
     return (model - data)/eps
@@ -102,78 +105,9 @@ def residual_fluid(pars, x, data=None, eps=None):
     l5 = pars['l5'].value
     l6 = pars['l6'].value
     
-    phy = pars['phy'].value
+    phy = pars['phy'].value # for pseudovoigt in a3, l3, f3
     
-    # Gaussian and pseudovoigt model
-    model = gaussian(x,a1,f1,l1) + \
-            gaussian(x,a2,f2,l2) + \
-            pseudovoigt(x,a3,f3,l3,phy) + \
-            gaussian(x,a4,f4,l4) + \
-            gaussian(x,a5,f5,l5) + \
-            gaussian(x,a6,f6,l6)
-    
-    if data is None:
-        return model
-    if eps is None:
-        return (model - data)
-    return (model - data)/eps
-
-def peaks_melt(pars, x):
-     # unpack parameters:
-    #  extract .value attribute for each parameter
-    a1 = pars['a1'].value
-    a2 = pars['a2'].value
-    a3 = pars['a3'].value
-    a4 = pars['a4'].value
-    a5 = pars['a5'].value
-    
-    f1 = pars['f1'].value
-    f2 = pars['f2'].value
-    f3 = pars['f3'].value
-    f4 = pars['f4'].value
-    f5 = pars['f5'].value    
-    
-    l1 = pars['l1'].value
-    l2 = pars['l2'].value
-    l3 = pars['l3'].value
-    l4 = pars['l4'].value
-    l5 = pars['l5'].value
-    
-    peak1 = gaussian(x,a1,f1,l1)
-    peak2 = gaussian(x,a2,f2,l2)
-    peak3 = gaussian(x,a3,f3,l3)
-    peak4 = gaussian(x,a4,f4,l4)
-    peak5 = gaussian(x,a5,f5,l5)
-    
-    return peak1,peak2,peak3,peak4,peak5
-    
-def peaks_fluid(pars, x):
-    # unpack parameters:
-    #  extract .value attribute for each parameter
-    a1 = pars['a1'].value
-    a2 = pars['a2'].value
-    a3 = pars['a3'].value
-    a4 = pars['a4'].value
-    a5 = pars['a5'].value
-    a6 = pars['a6'].value
-    
-    f1 = pars['f1'].value
-    f2 = pars['f2'].value
-    f3 = pars['f3'].value
-    f4 = pars['f4'].value
-    f5 = pars['f5'].value  
-    f6 = pars['f6'].value
-    
-    l1 = pars['l1'].value
-    l2 = pars['l2'].value
-    l3 = pars['l3'].value
-    l4 = pars['l4'].value
-    l5 = pars['l5'].value
-    l6 = pars['l6'].value
-    
-    phy = pars['phy'].value
-    
-    # Gaussian and pseudovoigt model
+     # Gaussian and pseudovoigt model
     peak1 = gaussian(x,a1,f1,l1)
     peak2 = gaussian(x,a2,f2,l2)
     peak3 = pseudovoigt(x,a3,f3,l3,phy)
@@ -181,8 +115,13 @@ def peaks_fluid(pars, x):
     peak5 = gaussian(x,a5,f5,l5)
     peak6 = gaussian(x,a6,f6,l6)
     
-    return peak1,peak2,peak3,peak4,peak5,peak6
-
+    model = peak1 + peak2 + peak3 + peak4 + peak5 + peak6
+    
+    if data is None:
+        return model, peak1, peak2, peak3, peak4, peak5, peak6
+    if eps is None:
+        return (model - data)
+    return (model - data)/eps
 
 ##### CORE OF THE CALCULATION BELOW
 
@@ -205,7 +144,7 @@ for lg in range(len(results)):
     sample = np.genfromtxt(name) # get the sample to deconvolute
 
     # we set here the lower and higher bonds for the interest region
-    lb = 450
+    lb = 730 ### MAY NEED TO AJUST THAT
     hb = 1250
     
     interestspectra = sample[np.where((sample[:,0] > lb)&(sample[:,0] < hb))]
@@ -220,50 +159,49 @@ for lg in range(len(results)):
     params = Parameters()
     ####################### FOR MELT:
     ####################### COMMENT IF NOT WANTED
-    #           (Name,  Value,  Vary,   Min,  Max,  Expr)
-#    params.add_many(('a1',   32,  True, 0,      None,  None),
-#               ('f1',   777,  True, 750,    None,  None),
-#               ('l1',   016,  True, 0,      None,  None),
-#               ('a2',   017,  True, 0,      None,  None),
-#               ('f2',   820,  True, None,   None,  None),
-#               ('l2',   032,  True, None,   None,  None),  
-#               ('a3',   40.51,True, 0,      None,  None),
-#               ('f3',   880,  True, None,   None,  None),
-#               ('l3',   044,  True, None,   None,  None),  
-#               ('a4',   041,  True, 0,      None,  None),
-#               ('f4',   1020, True, 900,    None,  None),
-#               ('l4',   047,  True, None,   None,  None),  
-#               ('a5',   077,  True, 0,      None,  None),
-#               ('f5',   1060, True, 1000,   None,  None),
-#               ('l5',   037,  True, None,   None,  None))  
+    #               (Name,  Value,  Vary,   Min,  Max,  Expr)
+    params.add_many(('a1',   32,   True, 0,      None,  None),
+                    ('f1',   777,  True, 750,    None,  None),
+                    ('l1',   016,  True, 0,      None,  None),
+                    ('a2',   017,  True, 0,      None,  None),
+                    ('f2',   820,  True, None,   None,  None),
+                    ('l2',   032,  True, None,   None,  None),  
+                    ('a3',   40,   True, 0,      None,  None),
+                    ('f3',   880,  True, None,   None,  None),
+                    ('l3',   45.3, True, None,   None,  'l2'),  
+                    ('a4',   041,  True, 0,      None,  None),
+                    ('f4',   1020, True, 900,    None,  None),
+                    ('l4',   047,  True, None,   None,  None),  
+                    ('a5',   077,  True, 0,      None,  None),
+                    ('f5',   1060, True, 1000,   None,  None),
+                    ('l5',   037,  True, None,   None,  None))  
     
     ####################### FOR FLUID:
     ####################### COMMENT IF NOT WANTED
     #           (Name,  Value,  Vary,   Min,  Max,  Expr)
-    params.add_many(('a1',   032,  True, 0,      None,  None),
-                    ('f1',   600,  True, None,   None,  None),
-                    ('l1',   020,  True, None,   None,  None),
-                    ('a2',   030,  True, 0,      None,  None),
-                    ('f2',   700,  True, None,   None,  None),
-                    ('l2',   031,  True, None,   None,  None),  
-                    ('a3',   80,  True, 0,      None,  None),
-                    ('f3',   763,  True, None,   None,  None),
-                    ('l3',   010,  True, None,   None,  None),  
-                    ('phy',  0.5,  True, 0,       1,    None),
-                    ('a4',   041,  True, 0,      None,  None),
-                    ('f4',   810,  True, 790,    None,  None),
-                    ('l4',   030,  True, None,   None,  None),  
-                    ('a5',   020,  True, 0,      None,  None),
-                    ('f5',   880,  True, 850,    None,  None),
-                    ('l5',   040,  True, None,   None,  None),  
-                    ('a6',   030,  True, 0,      None,  None),
-                    ('f6',   1060, True, 1000,   None,  None),
-                    ('l6',   030,  True, None,   None,  None))  
+#    params.add_many(('a1',   032,  True, 0,      None,  None),
+#                    ('f1',   600,  True, None,   None,  None),
+#                    ('l1',   020,  True, None,   None,  None),
+#                    ('a2',   030,  True, 0,      None,  None),
+#                    ('f2',   700,  True, None,   None,  None),
+#                    ('l2',   031,  True, None,   None,  None),  
+#                    ('a3',   80,  True, 0,      None,  None),
+#                    ('f3',   763,  True, None,   None,  None),
+#                    ('l3',   010,  True, None,   None,  None),  
+#                    ('phy',  0.5,  True, 0,       1,    None), # for pseudovoigt a3, l3, f3
+#                    ('a4',   041,  True, 0,      None,  None),
+#                    ('f4',   810,  True, 790,    None,  None),
+#                    ('l4',   030,  True, None,   None,  None),  
+#                    ('a5',   020,  True, 0,      None,  None),
+#                    ('f5',   880,  True, 850,    None,  None),
+#                    ('l5',   040,  True, None,   None,  None),  
+#                    ('a6',   030,  True, 0,      None,  None),
+#                    ('f6',   1060, True, 1000,   None,  None),
+#                    ('l6',   030,  True, None,   None,  None))  
     
-    result = minimize(residual_fluid, params, args=(xfit, data)) # fit data with leastsq model from scipy
-    yout = data + result.residual # the model line
+    result = minimize(residual_melt, params, args=(xfit, data)) # fit data with leastsq model from scipy
     model = fit_report(params) # the report
-    peak1,peak2,peak3,peak4,peak5,peak6 = peaks_fluid(params,xfit) # the different peaks
+    yout, peak1,peak2,peak3,peak4,peak5 = residual_melt(params,xfit) # the different peaks
     
     ##### WE DO A NICE FIGURE THAT CAN BE IMPROVED FOR PUBLICATION
     fig = figure()
@@ -274,7 +212,7 @@ for lg in range(len(results)):
     plot(xfit,peak3,'b-')
     plot(xfit,peak4,'b-')
     plot(xfit,peak5,'b-')
-    plot(xfit,peak6,'b-')
+    #plot(xfit,peak6,'b-')
     
     xlim(lb,hb)
     ylim(-5,105)
@@ -295,10 +233,9 @@ for lg in range(len(results)):
     pathout1 = pathbeg+pathint+namesample+ext1
     pathout2 = pathbeg+pathint+namesample+ext2
     pathout3 = pathbeg+pathint+namesample+ext3
-    np.savetxt(pathout1,np.concatenate((xfit,yout,peak1,peak2,peak3,peak4,peak5),1))
+    np.savetxt(pathout1,np.concatenate((xfit,data,yout,peak1,peak2,peak3,peak4,peak5),1))
     f = open(pathout2,'w')
     f.write(model)
-    f.close()    
-    #np.savetxt(pathout2,model)
+    f.close()
     savefig(pathout3)
  
