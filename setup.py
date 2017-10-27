b2c88db65ef22d9565f4bdfb189c6214c4aa04e9
Charles Le Losq <charles.lelosq@gmail.com>
2017-10-27 12:25:44 +1100

    rampy v0.2.4

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from numpy.distutils.core import setup
 from numpy.distutils.extension import Extension
 
 setup(name='rampy',
-      version='0.2.3',
+      version='0.2.4',
       description='A Python module containing functions to treat spectroscopic (XANES, Raman, IR...) data',
       url='https://github.com/charlesll/rampy',
       author='Charles Le Losq',
       author_email='charles.lelosq@anu.edu.au',
       license='GNU-GPLv2',
       packages=['rampy', 'rampy.tests'],
-      install_requires=['numpy>=1.12','gcvspline','scipy'],
+      install_requires=['numpy>=1.12','gcvspline','scipy','sklearn'],
       zip_safe=False)
